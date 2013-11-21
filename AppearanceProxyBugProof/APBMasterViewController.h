3eda4fae86ffd5a62cafc411460078298030b67c
Ellen Shapiro <designatednerd@gmail.com>
2013-11-21 12:04:00 -0600

    Initial Commit

---
+++ AppearanceProxyBugProof/APBMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  APBMasterViewController.h
+//  AppearanceProxyBugProof
+//
+//  Created by Ellen Shapiro on 11/21/13.
+//  Copyright (c) 2013 Vokal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APBMasterViewController : UITableViewController
+
+@end
